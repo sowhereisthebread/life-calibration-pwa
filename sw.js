51c673bf37c498bc1100a5a460a38b4402247e92
Tako <tako@local>
2026-08-10 16:03:32 +0800

    fix task secondary action grid scope

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "life-calibration-v0.4.13";
+const CACHE_NAME = "life-calibration-v0.4.14";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./style.css?v=0.4.13",
+  "./style.css?v=0.4.14",
   "./data-core.js",
   "./data-store.js",
-  "./app.js?v=0.4.13",
+  "./app.js?v=0.4.14",
   "./manifest.json",
   "./icons/icon-192.png?v=0.3.4",
   "./icons/icon-512.png",
